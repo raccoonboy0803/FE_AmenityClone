561cf8956f8611237a081fb9db825c9b7301d33a
raccoonboy <raccoonboy0803@gmail.com>
2023-05-13 12:38:24 +0900

    Init: tailwind, twinmacro 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
-function App() {}
+function App() {
+  return;
+}
 
 export default App;
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,12 @@
+import tw, { styled } from 'twin.macro';
+
+// const ToggleInput = styled.input`
+//   ${tw`
+//     focus:outline-none
+//     w-6
+//     h-6
+//     rounded-full
+//     absolute
+//     bg-red-800
+//    `}
+// `; 사용 예시
