8c01bab05689ada7276877ae2a69d8668c184344
SAEMMM <saem030@naver.com>
2023-05-13 12:46:10 +0900

    Init: 스타일 컴포넌트 import

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components'; // eslint-disable-line no-unused-vars
+
+function Header() {
+  return <div>header</div>;
+}
+
+export default Header;
