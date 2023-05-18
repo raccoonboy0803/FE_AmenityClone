c0b3fcae02f1f48afeceb63a86c7f35b5e4ee7aa
SAEMMM <saem030@naver.com>
2023-05-18 16:01:10 +0900

    Chore: .env 설정

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export default axios.create({
-  baseURL: 'http://3.36.65.197:8080/',
+  baseURL: process.env.REACT_APP_URL,
 });
 
 axios.interceptors.request.use(
